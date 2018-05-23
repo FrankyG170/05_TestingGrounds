1884b0a8f55bb83b2cb1a8e1d6704b0568099628
FrankyG170 <fran922003@yahoo.com>
2018-05-22 20:02:06 -0400

    Full patrolling functionality now exists solely through C++.

--- Source/S05_TestingGrounds/PatrollingGuard.h
+++ Source/S05_TestingGrounds/PatrollingGuard.h
@@ -14,7 +14,7 @@ class S05_TESTINGGROUNDS_API APatrollingGuard : public ATP_ThirdPersonCharacter
 	GENERATED_BODY()
 	
 
-protected:
+public: //TODO Remove or create Get function
 	UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
 	TArray <AActor*> PatrolPointsCPP;
 };
